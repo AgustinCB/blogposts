7b31e502aa64394e733a3e8b5fa40991677b52ec
Agustin Chiappe Berrini <jnieve@gmail.com>
2019-01-29 06:44:35 -0500

    Sketch of the first attempt

--- conferences/rust_meetup/20190129/first_try/src/lib.rs
+++ conferences/rust_meetup/20190129/first_try/src/lib.rs
@@ -1,7 +1,20 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+trait InputDevice {}
+trait OutputDevice {}
+
+struct ButtonDevice {}
+impl InputDevice for ButtonDevice {}
+
+struct SoundDevice {}
+impl OutputDevice for SoundDevice {}
+
+
+struct Cpu {
+    inputs: Vec<Box<InputDevice>>,
+    outputs: Vec<Box<OutputDevice>>,
+}
+
+struct Console {
+    cpu: Cpu,
+    sounds: Vec<SoundDevice>,
+    buttons: Vec<ButtonDevice>,
 }
