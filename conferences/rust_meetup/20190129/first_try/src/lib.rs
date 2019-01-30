a09e6d9218cb2b2c1934f3514b992aa241048e8d
Agustin Chiappe Berrini <jnieve@gmail.com>
2019-01-30 06:40:18 -0500

    first try

--- conferences/rust_meetup/20190129/first_try/src/lib.rs
+++ conferences/rust_meetup/20190129/first_try/src/lib.rs
@@ -9,12 +9,36 @@ impl OutputDevice for SoundDevice {}
 
 
 struct Cpu {
-    inputs: Vec<Box<InputDevice>>,
-    outputs: Vec<Box<OutputDevice>>,
+    pub inputs: Vec<Box<InputDevice>>,
+    pub outputs: Vec<Box<OutputDevice>>,
 }
 
 struct Console {
     cpu: Cpu,
     sounds: Vec<SoundDevice>,
     buttons: Vec<ButtonDevice>,
+}
+
+impl Console {
+    fn new() -> Console {
+        let mut cpu = Cpu {
+            inputs: Vec::new(),
+            outputs: Vec::new(),
+        };
+        let sound1 = SoundDevice {};
+        let sound2 = SoundDevice {};
+        let sounds = vec![sound1, sound2];
+        cpu.inputs.push(Box::new(sound1));
+        cpu.inputs.push(Box::new(sound2));
+        let button1 = ButtonDevice {};
+        let button2 = ButtonDevice {};
+        let buttons = vec![button1, button2];
+        cpu.inputs.push(Box::new(button1));
+        cpu.inputs.push(Box::new(button2));
+        Console {
+            cpu,
+            sounds,
+            buttons,
+        }
+    }
 }
