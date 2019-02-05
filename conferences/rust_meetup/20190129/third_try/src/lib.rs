bb9720f3499bb3e8b986c9abc91d814cf1ac0696
Agustin Chiappe Berrini <jnieve@gmail.com>
2019-02-05 06:35:02 -0500

    third try

---
+++ conferences/rust_meetup/20190129/third_try/src/lib.rs
@@ -0,0 +1,61 @@
+use std::rc::Rc;
+
+trait InputDevice {}
+trait OutputDevice {}
+
+enum Button {
+    Coin,
+    Left,
+    Right,
+    Shoot,
+    Start,
+}
+
+struct ButtonDevice {
+    pressed: Option<Button>,
+}
+impl InputDevice for ButtonDevice {
+    fn press_button(&mut self, button: Button) {
+        self.pressed = Some(button);
+    }
+}
+
+struct SoundDevice {}
+impl OutputDevice for SoundDevice {}
+
+
+struct Cpu {
+    pub inputs: Vec<Rc<InputDevice>>,
+    pub outputs: Vec<Rc<OutputDevice>>,
+}
+
+struct Console {
+    cpu: Cpu,
+    sounds: Vec<Rc<SoundDevice>>,
+    buttons: Vec<Rc<ButtonDevice>>,
+}
+
+impl Console {
+    fn new() -> Console {
+        let mut cpu = Cpu {
+            inputs: Vec::new(),
+            outputs: Vec::new(),
+        };
+        let sound1 = Rc::new(SoundDevice {});
+        let sound2 = Rc::new(SoundDevice {});
+        let sounds = vec![sound1.clone(), sound2.clone()];
+        cpu.outputs.push(sound1.clone());
+        cpu.outputs.push(sound2.clone());
+        let button1 = Rc::new(ButtonDevice {});
+        let mut button2 = Rc::new(ButtonDevice {});
+        let buttons = vec![button1.clone(), button2.clone()];
+        cpu.inputs.push(button1.clone());
+        cpu.inputs.push(button2.clone());
+        buttons2.press_button(Button::Coin);
+        Console {
+            cpu,
+            sounds,
+            buttons,
+        }
+    }
+}
